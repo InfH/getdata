32b029093365564502045cd69b53b27110605708
InfH <InfH@users.noreply.github.com>
2016-09-11 16:20:22 -0700

    minor updates

--- analysis.R
+++ analysis.R
@@ -1,29 +1,47 @@
-download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "data.zip")
-unzip("data.zip")
+#' Analysis of UCI HAR dataset
+#' Date modified: 10 August 2016
+
+# Downloading and extracting data files (run only once)
+download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "data.zip") 
+unzip("data.zip") 
+
+# Explore directory structure
 list.files()
 list.dirs()
 list.files("UCI HAR Dataset")
 list.files("UCI HAR Dataset/test")
-test <- read.table("UCI HAR Dataset/test/X_test.txt")
-testy <- read.table("UCI HAR Dataset/test/y_test.txt")
-testsub <- read.table("UCI HAR Dataset/test/subject_test.txt")
-train <- read.table("UCI HAR Dataset/train/X_train.txt")
-trainy <- read.table("UCI HAR Dataset/train/y_train.txt")
-trainsub <- read.table("UCI HAR Dataset/train/subject_train.txt")
+list.files("UCI HAR Dataset/train")
+
+# Load data into R
+
+# Get test data: measurements, activity labels, subject labels
+test <- read.table("UCI HAR Dataset/test/X_test.txt") # measurements
+testy <- read.table("UCI HAR Dataset/test/y_test.txt") # activity labels
+testsub <- read.table("UCI HAR Dataset/test/subject_test.txt") # subject labels
+
+# Get training data: measurements, activity labels, subject labels
+train <- read.table("UCI HAR Dataset/train/X_train.txt") # measurements
+trainy <- read.table("UCI HAR Dataset/train/y_train.txt") # activity labels
+trainsub <- read.table("UCI HAR Dataset/train/subject_train.txt") # subject
+
+# Vertically concatenate the measurement data into a new data.frame and add measurement names to identify mean / std
 alld <- rbind(test,train)
-colnames(alld) <- make.names(unlist(read.table("UCI HAR Dataset/features.txt", row.names = 1)), unique = TRUE)
+colnames(alld) <- make.names(unlist(read.table("UCI HAR Dataset/features.txt", row.names = 1)), unique = TRUE) # add column names from the feature key, make.names is to deal with some repeated names, though it does insert a lot of periods
 
-coltosel <- grep("mean|std",colnames(alld))
-dat_ms <- select(alld, coltosel )
-colnames(dat_ms) <- unlist(read.table("UCI HAR Dataset/features.txt", row.names = 1))[coltosel]
-dat_ms <- cbind(activity=rbind(testy,trainy),subject=rbind(testsub,trainsub),dat_ms)
-colnames(dat_ms)[1:2] <- c("activity", "subject")
+# Select mean / std measurements into a new data frame, dat_ms
+coltosel <- grep("mean|std",colnames(alld)) # get indices of columns of interest
+dat_ms <- select(alld, coltosel) # get columns of interest into new df 
+colnames(dat_ms) <- unlist(read.table("UCI HAR Dataset/features.txt", row.names = 1))[coltosel] # get more readable column names for columns of interest
 
-act_key <- read.table("UCI HAR Dataset/activity_labels.txt",col.names=c("activity", "activity_label"))
+# Add activity and subject labels to the front of the data frame
+dat_ms <- cbind(activity=rbind(testy,trainy),subject=rbind(testsub,trainsub),dat_ms) 
+colnames(dat_ms)[1:2] <- c("activity", "subject") # rename those columns
 
-dat_ms <- plyr::join(dat_ms,act_key,by='activity')
-dat_ms <- dat_ms[,c(ncol(dat_ms),3:ncol(dat_ms)-1)]
+# Relabel activity according to activity labels file
+act_key <- read.table("UCI HAR Dataset/activity_labels.txt",col.names=c("activity", "activity_label")) # read in key, with own column names
+dat_ms <- plyr::join(dat_ms,act_key,by='activity') # merge data frames using the activity column
+dat_ms <- dat_ms[,c(ncol(dat_ms),3:ncol(dat_ms)-1)] # reorder columns, so the last column (activity) is in front, then rest of columns 
 
+# Save the mean value for each measurement, by activity and subject in a new table
 tidy_dat <- dat_ms %>% group_by(activity_label,subject) %>% summarize_each("mean")
-
-write.table(tidy_dat, file="tidy_dat.txt", row.names = TRUE,col.names = TRUE,quote=FALSE, sep="\t")
+write.table(tidy_dat, file="tidy_dat.txt", row.names = FALSE,col.names = TRUE,quote=FALSE, sep="\t")
